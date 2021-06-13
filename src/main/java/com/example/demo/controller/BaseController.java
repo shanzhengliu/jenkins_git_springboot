06f18da393f8e27f394506645a8f7b955084176a
sam <lsz81241681@gmail.com>
2021-06-13 11:50:31 +0800

    jenkinsfile

--- src/main/java/com/example/demo/controller/BaseController.java
+++ src/main/java/com/example/demo/controller/BaseController.java
@@ -5,15 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
-        @GetMapping("/")
-        public String index()
-        {
-         return "hello world";
-        }
+    @GetMapping("/")
+    public String index()
+    {
+     return "hello world";
+    }
 
+    //region test
     @GetMapping("/test")
     public String test()
     {
         return "test";
     }
+    //endregion
 }
